c62e89392ae8dd20fc990428290b88f7d84dd2e8
abhishekdkulkarni1997 <poornanandnaik24ml@gmail.com>
2026-03-16 13:51:08 +0530

    Update Firebase configuration with new project details

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpJhLob1qfofj_KM5_o4M8D4HRLZmzY4s",
-    authDomain: "portfolio-2dfd0.firebaseapp.com",
-    projectId: "portfolio-2dfd0",
-    storageBucket: "portfolio-2dfd0.firebasestorage.app",
-    messagingSenderId: "484499494406",
-    appId: "1:484499494406:web:ff8b1ba729f4594e021587"
+  apiKey: "AIzaSyA0AW_OBMjbG4isGLizqT0vhFb8Yu5Q3zo",
+  authDomain: "portfolio-26709.firebaseapp.com",
+  projectId: "portfolio-26709",
+  storageBucket: "portfolio-26709.firebasestorage.app",
+  messagingSenderId: "535742441601",
+  appId: "1:535742441601:web:9b18b7db2a394410180580"
 };
 
 // Initialize Firebase
@@ -15,3 +15,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+
+
+
